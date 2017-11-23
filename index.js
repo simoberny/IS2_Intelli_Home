013a8c4c72f35a5bbdf1f75d5a4e07284018f517
Bernabe Simone (ACC0107439) <simone.bernabe@sci-a201pc62x.unitn.it>
2017-11-23 13:44:11 +0100

    -Generazione moduli

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-var apiai = require('apiai');
 var express = require('express');
+var session = require('express-session');
 var url = require('url');
-const http = require('http');
+var http = require('http');
+var speech = require('./speech.js');
+
 const host = 'api.worldweatheronline.com';
 const wwoApiKey = 'fda2e356d0ba46ecbc7153434171511';
 
 var apps = express();
-var app = apiai("1d6aa22653f341be840ff33c0f2dda0c");
-var session = require('express-session');
 
 apps.use(session({ secret: 'quellochevuoi', resave: true, saveUninitialized: true}));
 
@@ -24,99 +24,22 @@ apps.get('/', function(req, res) {
     });    
 });
 
-apps.get('/ajax', function(req, res) {
-    if(!req.session.message){
-        req.session.message = [];
-    }
+apps.use('/ajax', speech);
 
+apps.get('/attuatori', function(req, res) {
     res.writeHead(200, {'Content-Type': 'text/html'});
 
-    var data = req.query.data;
+    var evento = req.query.evento;
+    var stato = req.query.stato;
 
-    req.session.message.push({position: 'me', text: data});
+    var resp = "<span style=\"color: red;\">AJAX:</span> " + evento + " - Stato evento: " + stato;
 
-    var request = app.textRequest(data, {
-        sessionId: '5'
-    });
-
-    request.on('response', function(response) { 
-        let city = response.result.parameters['geo-city'];
-        let date = response.result.parameters['date'];
-        console.log("Città: " + city);
-        console.log('Data: ' + date);
-
-        if(!response.result.parameters['geo-city'] || !response.result.parameters['date']){
-            req.session.message.push({position: 'comp', text: response.result.fulfillment.speech});
-            res.write(response.result.fulfillment.speech);
-            res.end();
-            //console.log(response);
-        }else{
-            callWeatherApi(city, date).then((output) => {      
-                req.session.message.push({position: 'comp', text: output.output, table: true, geo: city, icon: output.icon});
-                
-                var htmlWeather = '' + 
-                '<div class="card">' +
-                    '<div class="card-body">' +
-                        '<h4 class="card-title">Meteo' + city + '</h4>' +
-                        '<h6 class="card-subtitle mb-2 text-muted">Weather Online</h6>' +
-                        '<img src="'+ output.icon +'" alt="..." class="img-thumbnail">' +
-                        '<p class="card-text">' + output.output + '</p>' +
-                    '</div>'+
-                '</div>';
-
-                res.write(htmlWeather);
-                res.end();
-            }).catch((error) => {
-                console.log("Errore: " + error)
-            });
-        }
-    });
-    
-    request.on('error', function(error) {
-        console.log(error);
-    });
-
-    request.end();
+    res.write(resp);
+    res.end();
 });
 
 apps.listen(apps.get('port'), function() {
     console.log('App is running, server is listening on port ', apps.get('port'));
 });
 
-function callWeatherApi (city, date) {
-    return new Promise((resolve, reject) => {
-      // Create the path for the HTTP request to get the weather
-      let path = '/premium/v1/weather.ashx?format=json&num_of_days=1' +
-        '&q=' + encodeURIComponent(city) + '&key=' + wwoApiKey + '&date=' + date + '&lang=it';
-
-      console.log('API Request: ' +  path);
-
-      http.get({host: host, path: path}, (res) => {
-        let body = ''; // var to store the response chunks
-        res.on('data', (d) => { body += d; }); // store each response chunk
-        res.on('end', () => {
-          // After all the data has been received parse the JSON for desired data
-          let response = JSON.parse(body);
-          let forecast = response['data']['weather'][0];
-          let location = response['data']['request'][0];
-          let conditions = response['data']['current_condition'][0];
-          let currentConditions = conditions['weatherDesc'][0]['value'];
-          let icon = conditions['weatherIconUrl'][0]['value'];
-          let italian = conditions['lang_it'][0]['value'];
-
-          // Create response
-          let output = `Il meteo nella ${location['type']} 
-          ${location['query']} è ${italian} con le temperature massime previste di
-          ${forecast['maxtempC']}°C e minime di
-          ${forecast['mintempC']}°C in data
-          ${forecast['date']}.`;
-          // Resolve the promise with the output text
-          resolve({output: output, icon: icon});
-        });
-        res.on('error', (error) => {
-          reject(error);
-        });
-      });
-    });
-  }
 
