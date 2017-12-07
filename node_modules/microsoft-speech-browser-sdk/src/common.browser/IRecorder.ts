80c4283817d2984f0cdc072efd46961deea3918d
simoberny <simone.bernabe@studenti.unitn.it>
2017-12-07 11:49:57 +0100

    Service Worker e Progressiva App

--- node_modules/microsoft-speech-browser-sdk/src/common.browser/IRecorder.ts
+++
@@ -1,6 +0,0 @@
-import { Stream } from "../common/Exports";
-
-export interface IRecorder {
-    Record(mediaStream: MediaStream, outputStream: Stream<ArrayBuffer>): void;
-    ReleaseMediaResources(): void;
-}
